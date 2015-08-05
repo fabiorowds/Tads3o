d7cc1c71f6dee4dcabe77e63a3b613cf3fa7ba0c
fabiorowds <fabiorowds@gmail.com>
2015-08-05 19:46:15 -0300

    Parcial

--- src/Qualquer.java
+++ src/Qualquer.java
@@ -8,6 +8,14 @@ public class Qualquer {
 
 	public static void main(String[] args) {
 		System.out.println("Olá mundo!");
+		
+		int res = calc(2,2);
+		
+		System.out.println(res);
+	}
+	
+	public static int calc(int a, int b) {
+		return a+b;
 	}
 	
 }
